b3ed8e7e142c6fd2b02b9d648ae516cb29119230
Lara Rodrigues <98907219+LRz00@users.noreply.github.com>
2025-05-27 10:02:38 -0300

    build: project set-up

---
+++ src/main/java/com/ifba/proj_inov/ProjInovApplication.java
@@ -0,0 +1,13 @@
+package com.ifba.proj_inov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjInovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjInovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifba/proj_inov/ProjInovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifba.proj_inov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjInovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
