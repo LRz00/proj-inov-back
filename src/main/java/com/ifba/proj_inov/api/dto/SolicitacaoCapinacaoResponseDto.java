72d218a49694e6196c8661c0406419753740b3e8
Mendes-Matheus <msmmendes211@gmail.com>
2025-07-14 15:52:34 -0300

    feat: adiciona solicitação de capinação

---
+++ src/main/java/com/ifba/proj_inov/api/dto/SolicitacaoCapinacaoResponseDto.java
@@ -0,0 +1,92 @@
+package com.ifba.proj_inov.api.dto;
+
+import com.ifba.proj_inov.core.entitites.Usuario;
+import com.ifba.proj_inov.core.entitites.enums.SolicitacaoStatusEnum;
+
+public class SolicitacaoCapinacaoResponseDto {
+
+    private Long id;
+    private String descricao;
+    private String dataCriada;
+    private SolicitacaoStatusEnum status;
+    private Usuario solicitante;
+    private String comentarios;
+    private String bairro;
+    private String nomeRua;
+
+    public SolicitacaoCapinacaoResponseDto() {
+    }
+
+    public SolicitacaoCapinacaoResponseDto(Long id, String descricao, String dataCriada, SolicitacaoStatusEnum status, Usuario solicitante, String comentarios) {
+        this.id = id;
+        this.descricao = descricao;
+        this.dataCriada = dataCriada;
+        this.status = status;
+        this.solicitante = solicitante;
+        this.comentarios = comentarios;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDataCriada() {
+        return dataCriada;
+    }
+
+    public void setDataCriada(String dataCriada) {
+        this.dataCriada = dataCriada;
+    }
+
+    public SolicitacaoStatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(SolicitacaoStatusEnum status) {
+        this.status = status;
+    }
+
+    public Usuario getSolicitante() {
+        return solicitante;
+    }
+
+    public void setSolicitante(Usuario solicitante) {
+        this.solicitante = solicitante;
+    }
+
+    public String getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(String comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getNomeRua() {
+        return nomeRua;
+    }
+
+    public void setNomeRua(String nomeRua) {
+        this.nomeRua = nomeRua;
+    }
+}
